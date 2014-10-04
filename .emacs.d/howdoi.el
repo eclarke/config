10fe9514781863311c9b885db7ec357751d114dc
Erik Clarke <ecl@mail.med.upenn.edu>
2014-10-04 14:28:14 -0400

    *** empty log message ***

--- .emacs.d/howdoi.el
+++ .emacs.d/howdoi.el
@@ -1,6 +1,10 @@
+;; Function to return results of howdoi inside the minibuffer.
+;; Author: Erik Clarke
+
 ;;;###autoload
 (defun howdoi (query)
   "Execute a howdoi query and return results in minibuffer"
   (interactive "sHow do I... ")
   (shell-command (format "howdoi -a %s" (shell-quote-argument query))))
 
+(provide 'howdoi)
