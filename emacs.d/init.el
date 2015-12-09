5b40d686e88178267f14183a57abaf09a8510301
Erik Clarke <erikclarke@gmail.com>
2015-12-09 13:13:18 -0500

    updated emacs config, added tmux config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2,7 +2,6 @@
 ;;  ecl general config file for emacs >=24
 ;;-----------------------------------------------------------------------;;
 
-
 ;;-----------------------------------------------------------------------;;
 ;;  General options
 ;;-----------------------------------------------------------------------;;
@@ -11,6 +10,39 @@
 (setq make-backup-files t)
 (setq backup-directory-alist (quote ((".*" . "~/.emacs_backups/"))))
 (setq inhibit-startup-message t)
+
+;; Unnecessary UI --> gone
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+;; No tabs
+(setq tab-width 2
+      indent-tabs-mode nil)
+;; Key bindings
+(global-set-key (kbd "RET") 'newline-and-indent)
+(global-set-key (kbd "C-;") 'comment-or-uncomment-region)
+
+;; Ido stuff
+(ido-mode t)
+(setq ido-enable-flex-matching t
+      ido-use-virtual-buffers)
+
+;; Temp files --> gone
+(setq backup-directory-alist `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
+
+
+;;-----------------------------------------------------------------------;;
+;;  package repos
+;;-----------------------------------------------------------------------;;
+(require 'package)
+;; Add melpa repository to archives
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(when (< emacs-major-version 24)
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+;; Initialize packages
+(package-initialize)
 
 ;;-----------------------------------------------------------------------;;
 ;;  el-get
@@ -25,10 +57,10 @@
 (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
 (el-get 'sync)
 
-
 ;;-----------------------------------------------------------------------;;
 ;;  pycheckers
 ;;-----------------------------------------------------------------------;;
+(require 'flymake)
 (add-hook 'python-mode-hook '(lambda () (flymake-mode)))
 ;; To avoid having to mouse hover for the error message, these functions 
 ;; make flymake error messages appear in the minibuffer
@@ -43,10 +75,42 @@
         (message "%s" (flymake-ler-text err)))))))
 (add-hook 'post-command-hook 'show-fly-err-at-point)
 
-
 ;;-----------------------------------------------------------------------;;
 ;;  External dependencies
 ;;-----------------------------------------------------------------------;;
 (ignore-errors
-  (load "~/config/emacs.d/howdoi")
-  (load "~/config/emacs.d/lab-notebook"))
+  (load "~/.config/emacs.d/howdoi")
+  (load "~/.config/emacs.d/lab-notebook"))
+
+;;-----------------------------------------------------------------------;;
+;;  Company mode
+;;-----------------------------------------------------------------------;;
+;; Enable company globally for all mode
+(global-company-mode)
+
+;; Reduce the time after which the company auto completion popup opens
+(setq company-idle-delay 0.2)
+
+;; Reduce the number of characters before company kicks in
+(setq company-minimum-prefix-length 1)
+
+;;-----------------------------------------------------------------------;;
+;;  Rust IDE stuff
+;;-----------------------------------------------------------------------;;
+
+
+;;-----------------------------------------------------------------------;;
+;;  Emacs-provided stuff
+;;-----------------------------------------------------------------------;;
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (wombat))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
